4df663e2c009b102cc7af362c3b1887ea2d5ef57
rajeshgopal <rajeshsmyl@gmail.com>
2017-12-04 14:35:04 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,14 +5,14 @@ include java
 include wget
 
 wget::fetch { 'artifactory-repo':
-  source      => 'https://bintray.com/jfrog/artifactory-pro-rpms/rpm',
+  source      => 'https://bintray.com/jfrog/artifactory-rpms/rpm',
   destination => '/etc/yum.repos.d/jfrog-artifactory.repo',
   timeout     => 0,
   verbose     => false,
   before      => Package['jfrog-artifactory-pro']
 }
 
-package { 'jfrog-artifactory-pro':
+package { 'jfrog-artifactory-oss':
   ensure => 'present',
   before => Service['artifactory']
 }
